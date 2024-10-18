0aac92da3454e80e68537904d51e3e92d2f95aa0
Topz <christofhermarkr@gmail.com>
2024-10-18 13:45:20 +0800

    Spacing in Home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <h2>Hi! <b>I'm Mark Christofher</b>, a web developer.</h2>
-      <p>Apassionate Web Developer that loves to convert web designs into an interactive and responsive website.</p>
+      <p>A passionate Web Developer that loves to convert web designs into an interactive and responsive website.</p>
     </div>
   );
 }
