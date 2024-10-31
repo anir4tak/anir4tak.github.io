0334e464aa6731bc11b28481fe1d086ffab66f59
Topz <christofhermarkr@gmail.com>
2024-11-01 06:38:00 +0800

    Header Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,9 +23,9 @@ const Header = () => {
           {isOpen && <div className="close-icon" onClick={toggleMenu}>✖</div>}
           <ul className={isOpen ? 'nav-links nav-none open' : 'nav-links'}>
             <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
             <li><a href="#skills">Skills</a></li>
             <li><a href="#experience">Experience</a></li>
-            <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact Me</a></li>
             {/* <li><a href={CV} download={CV} className="cv-download">Download my CV</a></li> */}
           </ul>
